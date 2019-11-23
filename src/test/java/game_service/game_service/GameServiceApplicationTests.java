034d38d38a1beb4a6a2a574173f0a0ce1fb99f42
mohabashraf <mohabashraf466@gmail.com>
2019-11-23 19:34:10 +0200

    inital

---
+++ src/test/java/game_service/game_service/GameServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package game_service.game_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
